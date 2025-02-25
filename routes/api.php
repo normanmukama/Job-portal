17656c1294f0f33df3f2d678b1c0f4c00ef44f9d
normanmukama <normanmukama11@gmail.com>
2025-02-25 16:24:03 +0300

    jobs endpoint

--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,13 @@ use App\Http\Controllers\FetchJobsController;
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/fetch-jobs', [JobController::class, 'index']);
 
 // Protected routes (requires authentication)
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('jobs', JobController::class)->only(['index', 'store']);
     Route::post('/applications', [JobApplicationController::class, 'store']);
-    Route::get('/fetch-jobs', [JobController::class, 'index']);
+    // Route::get('/fetch-jobs', [JobController::class, 'index']);
     Route::get('/jobs/{id}', [JobController::class, 'show']);
     Route::post('/jobs-create', [JobController::class, 'store']);
 });
